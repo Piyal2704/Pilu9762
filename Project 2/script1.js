1b9eb4d95619d884d3bae092bd40b21100d25d24
Piyal2704 <140900982+Piyal2704@users.noreply.github.com>
2025-11-09 13:56:40 +0530

    Add files via upload

---
+++ Project 2/script1.js
@@ -0,0 +1,8 @@
+document.getElementById("onBtn").addEventListener("click", function() {
+    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulbon.gif";
+  });
+  
+  document.getElementById("offBtn").addEventListener("click", function() {
+    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulboff.gif";
+  });
+  
