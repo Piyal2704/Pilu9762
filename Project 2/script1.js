1185fe631d95eb1063cf97dbc7acd65067d783b8
Piyal2704 <140900982+Piyal2704@users.noreply.github.com>
2025-11-09 14:16:58 +0530

    Delete Project 2 directory

--- Project 2/script1.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("onBtn").addEventListener("click", function() {
-    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulbon.gif";
-  });
-  
-  document.getElementById("offBtn").addEventListener("click", function() {
-    document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulboff.gif";
-  });
-  
